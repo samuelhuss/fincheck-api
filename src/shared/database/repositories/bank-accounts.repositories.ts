15d61019f7598494751d3f06bb1ae64c77683e59
huss <samuelhussdb@gmail.com>
2023-08-09 15:53:37 -0300

    'test'

---
+++ src/shared/database/repositories/bank-accounts.repositories.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@nestjs/common";
+import { type Prisma } from "@prisma/client";
+import { PrismaService } from "../prisma.service";
+
+@Injectable()
+export class BankAccountsRepository{
+    constructor(private readonly prismaService: PrismaService) {}
+
+
+    findMany<T extends Prisma.BankAccountFindManyArgs>( 
+        findManyDto: Prisma.SelectSubset<T, Prisma.BankAccountFindManyArgs>, 
+        ) {
+             return this.prismaService.bankAccount.findMany(findManyDto); 
+            }
+            
+    create(createDto: Prisma.BankAccountCreateArgs) {
+        return this.prismaService.bankAccount.create(createDto)
+    }
+
+    update(updateDto: Prisma.BankAccountUpdateArgs) {
+        return this.prismaService.bankAccount.update(updateDto)
+    }
+
+   
+
+    findFirst(findFirstDto: Prisma.BankAccountFindFirstArgs) {
+        return this.prismaService.bankAccount.findFirst(findFirstDto)
+    }
+
+    delete(deleteDto: Prisma.BankAccountDeleteArgs) {
+        return this.prismaService.bankAccount.delete(deleteDto)
+    }
+
+
+}
